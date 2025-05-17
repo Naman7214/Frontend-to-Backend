29675b8089cf099e193114b6dcf76c90a161989f
vaghani04 <vaghanimaunik03@gmail.com>
2025-05-17 21:50:59 +0530

    MongoDB Connection is done

--- src/app/config/settings.py
+++ src/app/config/settings.py
@@ -1,1 +1,16 @@
-# Placeholder for settings.py
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    # MongoDB settings
+    MONGODB_URL: str = "mongodb://localhost:27017"
+    MONGODB_DB_NAME: str = "F2B"
+    ERROR_COLLECTION_NAME: str = "error_logs"
+    LLM_USAGE_COLLECTION_NAME: str = "llm_usage_logs"
+
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
--- src/main.py
+++ src/main.py
@@ -1,1 +1,28 @@
-# Placeholder for main.py
+from contextlib import asynccontextmanager
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from src.app.config.database import mongodb_database
+
+
+@asynccontextmanager
+async def db_lifespan(app: FastAPI):
+    mongodb_database.connect()
+    yield
+    mongodb_database.disconnect()
+
+
+app = FastAPI(title="My FastAPI Application", lifespan=db_lifespan)
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8001"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to my FastAPI application!"}
